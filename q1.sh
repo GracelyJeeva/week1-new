094dc200758e03ce8ee7afa4a5233b6279073db7
poojitha0809 <poojitha.s@niser.nic.in>
2019-08-09 20:35:55 +0530

    done it

---
+++ q1.sh
@@ -0,0 +1,7 @@
+echo "enter your name:"
+read n
+mkdir $n 
+cd $n
+touch $n.cpp
+echo " your file has been created"
+
---
+++ q2.sh
@@ -0,0 +1,14 @@
+echo "enter your name:"
+read n
+mkdir $n
+cd $n
+touch "$n"1.cpp
+touch "$n"2.cpp
+touch "$n"3.cpp
+touch "$n"4.cpp
+touch "$n"5.cpp
+
+echo " your file has been created"
+
+
+
